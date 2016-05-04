5c997560d676c1fc40a55d77bc4340e3fc0f6a45
sanderjo <sander.jonkers+github@gmail.com>
2016-05-04 19:21:58 +0200

    Spaces, no Tabs

--- ipinfo.py
+++ ipinfo.py
@@ -24,6 +24,7 @@ import urllib
 baseurl = 'http://ipinfo.io/'    # no HTTPS supported (at least: not without a plan)
 
 
+
 def getall(ipaddress):
     url = '%s%s/json' % (baseurl, ipaddress)
     try:
