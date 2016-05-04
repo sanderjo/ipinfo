b5ce9a2dd6773059069fe0a9a0f7ff00d8401b5a
sanderjo <sander.jonkers+github@gmail.com>
2016-05-04 19:20:36 +0200

    Spaces, no Tabs

--- ipinfo.py
+++ ipinfo.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Gets info on IP address (IPv4 or IPv6) from http://ipinfo.io/
+# Source: https://github.com/sanderjo/ipinfo.git
+# GPL3
+
 '''
 Based on this public API from http://ipinfo.io/ :
 
@@ -17,51 +21,51 @@ $ curl http://ipinfo.io/31.21.30.159/json
 
 import json
 import urllib
-baseurl = 'http://ipinfo.io/'	# no HTTPS supported (without plan)
+baseurl = 'http://ipinfo.io/'    # no HTTPS supported (at least: not without a plan)
 
 
 def getall(ipaddress):
-	url = '%s%s/json' % (baseurl, ipaddress)
-	try:
-		urlresult = urllib.urlopen(url)
-		jsonresult = urlresult.read()		# get the JSON
-		parsedjson = json.loads(jsonresult)	# put parsed JSON into dictionary
-		return parsedjson
-	except:
-		return None
+    url = '%s%s/json' % (baseurl, ipaddress)
+    try:
+        urlresult = urllib.urlopen(url)
+        jsonresult = urlresult.read()          # get the JSON
+        parsedjson = json.loads(jsonresult)    # put parsed JSON into dictionary
+        return parsedjson
+    except:
+        return None
 
 
 def country_and_org(ipaddress):
-	allinfo = getall(ipaddress)	# one lookup
-	try:
-		# FYI: the first word in allinfo['org'] is the ASN, which we skip
-		return allinfo['country'] + ' --- ' + allinfo['org'].split(' ', 1)[1]
-	except:
-		return ""
+    allinfo = getall(ipaddress)    # one lookup
+    try:
+        # FYI: the first word in allinfo['org'] is the ASN, which we skip
+        return allinfo['country'] + ' --- ' + allinfo['org'].split(' ', 1)[1]
+    except:
+        return ""
 
 def country_and_org_as_list(ipaddress):
-	allinfo = getall(ipaddress)	# one lookup
-	try:
-		# FYI: the first word in allinfo['org'] is the ASN, which we skip
-		return [ allinfo['country'], allinfo['org'].split(' ', 1)[1] ]
-	except:
-		return ['','']
+    allinfo = getall(ipaddress)    # one lookup
+    try:
+        # FYI: the first word in allinfo['org'] is the ASN, which we skip
+        return [ allinfo['country'], allinfo['org'].split(' ', 1)[1] ]
+    except:
+        return ['','']
 
 if __name__ == '__main__':
 
-	# Some examples:
+    # Some examples:
 
-	print getall('31.21.30.159')
-	print country_and_org('31.21.30.159')
+    print getall('31.21.30.159')
+    print country_and_org('31.21.30.159')
 
-	print getall('192.168.0.1')
-	print country_and_org('192.168.0.1')
+    print getall('192.168.0.1')
+    print country_and_org('192.168.0.1')
 
-	print country_and_org('2a00:1450:4013:c01::64')
-	print country_and_org('::ffff:194.109.6.92')
+    print country_and_org('2a00:1450:4013:c01::64')
+    print country_and_org('::ffff:194.109.6.92')
 
-	print ' --- '.join(country_and_org_as_list('31.21.30.159'))
-	print ' --- '.join(country_and_org_as_list('31.21.30.'))
+    print ' --- '.join(country_and_org_as_list('31.21.30.159'))
+    print ' --- '.join(country_and_org_as_list('31.21.30.'))
 
 
 
